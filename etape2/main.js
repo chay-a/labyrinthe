9433aacefe32c970e03f61000e9de20eac84ca76
Chay Auriane <chaya.07@protonmail.com>
2022-07-04 16:30:56 +0200

    etape2 : second draft

--- etape2/main.js
+++ etape2/main.js
@@ -1,9 +1,9 @@
-const lab = [[[0, ""],[1, ""],[0, ""],[0, ""],[0, ""],[0, ""],[0, ""]], [[0, ""],[1, ""],[0, ""],[1, ""],[1, ""],[0, ""],[1, ""]], [[0, ""],[1, ""],[0, ""],[1, ""],[0, ""],[0, ""],[0, ""]], [[0, ""],[0, ""],[0, ""],[0, ""],[1, ""],[1, ""],[0, ""]], [[0, ""],[1, ""],[0, ""],[1, ""],[0, ""],[0, ""],[0, ""]], [[0, ""],[1, ""],[0, ""],[0, ""],[0, ""],[1, ""],[0, ""]]];
+const lab = [[[0, ""], [1, ""], [0, ""], [0, ""], [0, ""], [0, ""], [0, ""]], [[0, ""], [1, ""], [0, ""], [1, ""], [1, ""], [0, ""], [1, ""]], [[0, ""], [1, ""], [0, ""], [1, ""], [0, ""], [0, ""], [0, ""]], [[0, ""], [0, ""], [0, ""], [0, ""], [1, ""], [1, ""], [0, ""]], [[0, ""], [1, ""], [0, ""], [1, ""], [0, ""], [0, ""], [0, ""]], [[0, ""], [1, ""], [0, ""], [0, ""], [0, ""], [1, ""], [0, ""]]];
 const goal = [2, 4];
 let position = [0, 0];
 let i = 1;
 let j = 0;
-let ariane = [[0,0]];
+let ariane = [[0, 0]];
 
 console.table(lab);
 
@@ -17,28 +17,39 @@ function labyrinthe(lab, ariane, position) {
     if (position[0] == goal[0] && position[1] == goal[1]) {
         return;
     } else {
-        if (typeof lab[position[0]+1] !== 'undefined' && lab[position[0]+1][position[1]][0] == 0 && lab[position[0]+1][position[1]][1].charAt(0) != "D") {
-            lab[position[0]][position[1]][1] = "D";
+        checkPossibilities(lab, position);
+
+        if (lab[position[0]][position[1]][1].charAt(0) == "D") {
             position[0] += 1;
-
-        } else if (typeof lab[position[0]][position[1] + 1] !== 'undefined' && lab[position[0]][position[1] + 1][0] == 0 && lab[position[0]][position[1] + 1][1].charAt(1) != "R") {
-            lab[position[0]][position[1]][1] = "DR";
+        } else if (lab[position[0]][position[1]][1].charAt(0) == "R") {
             position[1] += 1;
-
-        } else if (typeof lab[position[0] - 1] !== 'undefined' && lab[position[0] - 1][position[1]][0] == 0 && lab[position[0] - 1][position[1]][1].charAt(2) != "DRU") {
-            lab[position[0]][position[1]][1] = "DRU";
+        } else if (lab[position[0]][position[1]][1].charAt(0) == "U") {
             position[0] -= 1;
-
-        } else if (typeof lab[position[0]][position[1] - 1] !== 'undefined' && lab[position[0]][position[1] - 1][0] == 0 && lab[position[0]][position[1] - 1][1].charAt(3) != "DRUL") {
-            lab[position[0]][position[1]][1] = "DRUL";
+        } else if (lab[position[0]][position[1]][1].charAt(0) == "L") {
             position[1] -= 1;
         } else {
-            position[0] = ariane[i-1][0];
-            position[1] = ariane[i-1][1];
+
         }
+        lab[position[0]][position[1]][1].slice(1);
+
         ariane[i] = [position[0], position[1]];
         i++;
         labyrinthe(lab, ariane, position);
     }
 
+}
+
+function checkPossibilities(lab, position) {
+    if (typeof lab[position[0] + 1] !== 'undefined' && lab[position[0] + 1][position[1]][0] == 0) {
+        lab[position[0]][position[1]][1] = "D";
+    }
+    if (typeof lab[position[0]][position[1] + 1] !== 'undefined' && lab[position[0]][position[1] + 1][0] == 0) {
+        lab[position[0]][position[1]][1] += "R";
+    }
+    if (typeof lab[position[0] - 1] !== 'undefined' && lab[position[0] - 1][position[1]][0] == 0) {
+        lab[position[0]][position[1]][1] += "U";
+    }
+    if (typeof lab[position[0]][position[1] - 1] !== 'undefined' && lab[position[0]][position[1] - 1][0] == 0) {
+        lab[position[0]][position[1]][1] += "L";
+    }
 }
