c0bb52e661bc8d6f45e4219f69afcd7b19393f92
Chay Auriane <chaya.07@protonmail.com>
2022-07-05 16:32:49 +0200

    etape2 : i think it works

--- etape2/main.js
+++ etape2/main.js
@@ -1,9 +1,15 @@
-const lab = [[[0, ""], [1, ""], [0, ""], [0, ""], [0, ""], [0, ""], [0, ""]], [[0, ""], [1, ""], [0, ""], [1, ""], [1, ""], [0, ""], [1, ""]], [[0, ""], [1, ""], [0, ""], [1, ""], [0, ""], [0, ""], [0, ""]], [[0, ""], [0, ""], [0, ""], [0, ""], [1, ""], [1, ""], [0, ""]], [[0, ""], [1, ""], [0, ""], [1, ""], [0, ""], [0, ""], [0, ""]], [[0, ""], [1, ""], [0, ""], [0, ""], [0, ""], [1, ""], [0, ""]]];
-const goal = [2, 4];
+const lab = [
+    [{ path: 0, possibilities: "", stepCount: 0, isPassed: false }, { path: 1, possibilities: "", stepCount: 0, isPassed: false }, { path: 0, possibilities: "", stepCount: 0, isPassed: false }, { path: 0, possibilities: "", stepCount: 0, isPassed: false }, { path: 0, possibilities: "", stepCount: 0, isPassed: false }, { path: 0, possibilities: "", stepCount: 0, isPassed: false }, { path: 0, possibilities: "", stepCount: 0, isPassed: false }],
+    [{ path: 0, possibilities: "", stepCount: 0, isPassed: false }, { path: 1, possibilities: "", stepCount: 0, isPassed: false }, { path: 0, possibilities: "", stepCount: 0, isPassed: false }, { path: 1, possibilities: "", stepCount: 0, isPassed: false }, { path: 1, possibilities: "", stepCount: 0, isPassed: false }, { path: 0, possibilities: "", stepCount: 0, isPassed: false }, { path: 1, possibilities: "", stepCount: 0, isPassed: false }],
+    [{ path: 0, possibilities: "", stepCount: 0, isPassed: false }, { path: 1, possibilities: "", stepCount: 0, isPassed: false }, { path: 0, possibilities: "", stepCount: 0, isPassed: false }, { path: 1, possibilities: "", stepCount: 0, isPassed: false }, { path: 0, possibilities: "", stepCount: 0, isPassed: false }, { path: 0, possibilities: "", stepCount: 0, isPassed: false }, { path: 0, possibilities: "", stepCount: 0, isPassed: false }],
+    [{ path: 0, possibilities: "", stepCount: 0, isPassed: false }, { path: 0, possibilities: "", stepCount: 0, isPassed: false }, { path: 0, possibilities: "", stepCount: 0, isPassed: false }, { path: 0, possibilities: "", stepCount: 0, isPassed: false }, { path: 1, possibilities: "", stepCount: 0, isPassed: false }, { path: 1, possibilities: "", stepCount: 0, isPassed: false }, { path: 0, possibilities: "", stepCount: 0, isPassed: false }],
+    [{ path: 0, possibilities: "", stepCount: 0, isPassed: false }, { path: 1, possibilities: "", stepCount: 0, isPassed: false }, { path: 0, possibilities: "", stepCount: 0, isPassed: false }, { path: 1, possibilities: "", stepCount: 0, isPassed: false }, { path: 0, possibilities: "", stepCount: 0, isPassed: false }, { path: 0, possibilities: "", stepCount: 0, isPassed: false }, { path: 0, possibilities: "", stepCount: 0, isPassed: false }],
+    [{ path: 0, possibilities: "", stepCount: 0, isPassed: false }, { path: 1, possibilities: "", stepCount: 0, isPassed: false }, { path: 0, possibilities: "", stepCount: 0, isPassed: false }, { path: 0, possibilities: "", stepCount: 0, isPassed: false }, { path: 0, possibilities: "", stepCount: 0, isPassed: false }, { path: 1, possibilities: "", stepCount: 0, isPassed: false }, { path: 0, possibilities: "", stepCount: 0, isPassed: false }]];
+const goal = [4, 2];
 let position = [0, 0];
-let i = 1;
-let j = 0;
-let ariane = [[0, 0]];
+let i = 0;
+let ariane = [];
+let stepCount = 0;
 
 console.table(lab);
 
@@ -17,39 +23,93 @@ function labyrinthe(lab, ariane, position) {
     if (position[0] == goal[0] && position[1] == goal[1]) {
         return;
     } else {
-        checkPossibilities(lab, position);
 
-        if (lab[position[0]][position[1]][1].charAt(0) == "D") {
-            position[0] += 1;
-        } else if (lab[position[0]][position[1]][1].charAt(0) == "R") {
-            position[1] += 1;
-        } else if (lab[position[0]][position[1]][1].charAt(0) == "U") {
-            position[0] -= 1;
-        } else if (lab[position[0]][position[1]][1].charAt(0) == "L") {
-            position[1] -= 1;
-        } else {
-
+        if (!lab[position[1]][position[0]].isPassed) {
+            checkPossibilities(lab, position);
         }
-        lab[position[0]][position[1]][1].slice(1);
+
+
+        let positionX = position[0];
+        let positionY = position[1];
+
+        if (lab[position[1]][position[0]].possibilities.charAt(0) == "D") {
+            positionY += 1;
+        } else if (lab[position[1]][position[0]].possibilities.charAt(0) == "R") {
+            positionX += 1;
+        } else if (lab[position[1]][position[0]].possibilities.charAt(0) == "U") {
+            positionY -= 1;
+        } else if (lab[position[1]][position[0]].possibilities.charAt(0) == "L") {
+            positionX -= 1;
+        } else {
+            let returnPossibilities = [];
+            let indexMax = findOldPath(lab, position, returnPossibilities);
+            positionX = returnPossibilities[indexMax].positionX;
+            positionY = returnPossibilities[indexMax].positionY;
+            stepCount -= 2;
+            lab[position[1]][position[0]].possibilities = "N" + lab[position[1]][position[0]].possibilities;
+        }
+
+        lab[position[1]][position[0]].possibilities = lab[position[1]][position[0]].possibilities.slice(1);
+
+        lab[position[1]][position[0]].stepCount = stepCount;
+        lab[position[1]][position[0]].isPassed = true;
+
+        position[0] = positionX;
+        position[1] = positionY;
+
+        stepCount++;
+        i++;
 
         ariane[i] = [position[0], position[1]];
-        i++;
+        // console.table(lab);
+        // console.log(position);
         labyrinthe(lab, ariane, position);
     }
 
 }
 
+function findOldPath(lab, position, returnPossibilities) {
+    if (typeof lab[position[1]][position[0] - 1] !== 'undefined' && lab[position[1]][position[0] - 1].path == 0 && lab[position[1]][position[0] - 1].isPassed && !(lab[position[1]][position[0] - 1].possibilities.includes('R'))) {
+        returnPossibilities.push({ positionX: position[0] - 1, positionY: position[1], possibilities: lab[position[1]][position[0] - 1].possibilities });
+    }
+    if (typeof lab[position[1] - 1] !== 'undefined' && lab[position[1] - 1][position[0]].path == 0 && lab[position[1] - 1][position[0]].isPassed && !(lab[position[1] - 1][position[0]].possibilities.includes("D"))) {
+        returnPossibilities.push({ positionX: position[0], positionY: position[1] - 1, possibilities: lab[position[1] - 1][position[0]].possibilities });
+    }
+    if (typeof lab[position[1]][position[0] + 1] !== 'undefined' && lab[position[1]][position[0] + 1].path == 0 && lab[position[1]][position[0] + 1].isPassed && !(lab[position[1]][position[0] + 1].possibilities.includes('L'))) {
+        returnPossibilities.push({ positionX: position[0] + 1, positionY: position[1], possibilities: lab[position[1]][position[0] + 1].possibilities });
+    }
+    if (typeof lab[position[1] + 1] !== 'undefined' && lab[position[1] + 1][position[0]].path == 0 && lab[position[1] + 1][position[0]].isPassed && !(lab[position[1] + 1][position[0]].possibilities.includes('U'))) {
+        returnPossibilities.push({ positionX: position[0], positionY: position[1] + 1, possibilities: lab[position[1] + 1][position[0]].possibilities });
+    }
+
+    let indexMax = 0;
+    for (let index = 0; index < returnPossibilities.length; index++) {
+        console.log(ariane[i-1][0] != returnPossibilities[index].positionX && ariane[i-1][1] != returnPossibilities[index].positionY);
+        console.log(ariane[i-1][0] == returnPossibilities[index].positionX && ariane[i-1][1] != returnPossibilities[index].positionY);
+        console.log(ariane[i-1][0] != returnPossibilities[index].positionX && ariane[i-1][1] == returnPossibilities[index].positionY);
+        console.log(((ariane[i-1][0] != returnPossibilities[index].positionX && ariane[i-1][1] != returnPossibilities[index].positionY) || (ariane[i-1][0] == returnPossibilities[index].positionX && ariane[i-1][1] != returnPossibilities[index].positionY) ||  (ariane[i-1][0] != returnPossibilities[index].positionX && ariane[i-1][1] == returnPossibilities[index].positionY)));
+        console.log(returnPossibilities[index].possibilities.length > returnPossibilities[indexMax].possibilities.length);
+        if ( ((ariane[i-1][0] != returnPossibilities[index].positionX && ariane[i-1][1] != returnPossibilities[index].positionY) || (ariane[i-1][0] == returnPossibilities[index].positionX && ariane[i-1][1] != returnPossibilities[index].positionY) ||  (ariane[i-1][0] != returnPossibilities[index].positionX && ariane[i-1][1] == returnPossibilities[index].positionY)) && returnPossibilities[index].possibilities.length >= returnPossibilities[indexMax].possibilities.length) {
+            indexMax = index;
+        }
+    }
+    return indexMax;
+}
+
 function checkPossibilities(lab, position) {
-    if (typeof lab[position[0] + 1] !== 'undefined' && lab[position[0] + 1][position[1]][0] == 0) {
-        lab[position[0]][position[1]][1] = "D";
+    if (typeof lab[position[1] + 1] !== 'undefined' && lab[position[1] + 1][position[0]].path == 0 && !lab[position[1] + 1][position[0]].isPassed) {
+        lab[position[1]][position[0]].possibilities = "D";
     }
-    if (typeof lab[position[0]][position[1] + 1] !== 'undefined' && lab[position[0]][position[1] + 1][0] == 0) {
-        lab[position[0]][position[1]][1] += "R";
+    if (typeof lab[position[1]][position[0] + 1] !== 'undefined' && lab[position[1]][position[0] + 1].path == 0 && !lab[position[1]][position[0] + 1].isPassed) {
+        lab[position[1]][position[0]].possibilities += "R";
     }
-    if (typeof lab[position[0] - 1] !== 'undefined' && lab[position[0] - 1][position[1]][0] == 0) {
-        lab[position[0]][position[1]][1] += "U";
+    if (typeof lab[position[1] - 1] !== 'undefined' && lab[position[1] - 1][position[0]].path == 0 && !lab[position[1] - 1][position[0]].isPassed) {
+        lab[position[1]][position[0]].possibilities += "U";
     }
-    if (typeof lab[position[0]][position[1] - 1] !== 'undefined' && lab[position[0]][position[1] - 1][0] == 0) {
-        lab[position[0]][position[1]][1] += "L";
+    if (typeof lab[position[1]][position[0] - 1] !== 'undefined' && lab[position[1]][position[0] - 1].path == 0 && !lab[position[1]][position[0] - 1].isPassed) {
+        lab[position[1]][position[0]].possibilities += "L";
     }
+    // if (lab[position[1]][position[0]].possibilities == "") {
+    //     lab[position[1]][position[0]].possibilities += "N";
+    // }
 }
